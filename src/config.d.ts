b38566ca8ce3155c2251ab0bfca7b9584c5a4c0c
Andrew Thal <467872+athal7@users.noreply.github.com>
2021-09-13 15:48:15 +0000

    closes #29; Fix remote start settings
    And validate using the exported type interface from Bluelinky
    
    Signed-off-by: GitHub <noreply@github.com>

--- src/config.d.ts
+++ src/config.d.ts
@@ -1,27 +1,23 @@
-import { REGION } from 'bluelinky/dist/constants'
-import { Brand } from 'bluelinky/dist/interfaces/common.interfaces'
-import { PlatformConfig } from 'homebridge'
+import { REGION } from "bluelinky/dist/constants";
+import {
+  Brand,
+  VehicleStartOptions,
+} from "bluelinky/dist/interfaces/common.interfaces";
+import { PlatformConfig } from "homebridge";
 
 interface AuthConfig {
-    username: string
-    password: string
-    pin: string
-    region: REGION
-    brand: Brand
+  username: string;
+  password: string;
+  pin: string;
+  region: REGION;
+  brand: Brand;
 }
 interface VehicleConfig {
-    vin: string
-    maxRange?: number
-}
-interface StartConfig {
-    cool?: boolean
-    heat?: boolean
-    defrost?: boolean
-    temperature?: number
-    igniOnDuration: number
+  vin: string;
+  maxRange?: number;
 }
 export interface HyundaiConfig extends PlatformConfig {
-    credentials: AuthConfig
-    vehicles: VehicleConfig[]
-    remoteStart: StartConfig
+  credentials: AuthConfig;
+  vehicles: VehicleConfig[];
+  remoteStart: VehicleStartOptions;
 }
